c482be44479e3b44b4cc8b3948ce2e5bce3784d4
thatsathing <thatsathing@users.noreply.github.com>
2017-06-14 19:00:11 -0400

    An actual version, not that excuse I made earlier
    Comes with the bouncing balls mod installed

--- sketch.js
+++ sketch.js
@@ -1,16 +1,89 @@
+var borderWidth;
 var fontSize = 72;
-var d = new Date();
-var x = 0;
-var y = 0;
+
+var d;
+
+/*  COLORS
+ *  From material.io
+*/
+
+var primMain;
+var primLight;
+var primDark;
+var primText;
+
+var secMain;
+var secLight;
+var secDark;
+var secText;
+
+function defineColors() {
+    primMain = color('#ff9800')
+    primLight = color('#ffc947')
+    primDark = color('#c66900')
+    primText = color('#000000')
+
+    secMain = color('#212121')
+    secLight = color('#585858')
+    secDark = color('#000000')
+    secText = color('#ffffff')
+}
+
+var dispDay = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+
+var dispMonth = [
+    "January",
+    "Februrary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
 function setup() {
-  createCanvas(windowWidth-4,windowHeight-4);
+    createCanvas(windowWidth-4,windowHeight-4);
+    borderWidth = floor(width/20);
+    textSize(fontSize);
+    noStroke();
+    defineColors();
+    d = new(Date);
+
+    //  MODS
+    modBallsSetup();
+
 }
 
 function draw() {
+    background(primDark);
+    fill(primLight);
+    rect(borderWidth,borderWidth,width-(borderWidth*2),height-(borderWidth*2))
 
-  textSize(fontSize);
-  textAlign(CENTER);
-  text("Lafayete",width/2, height/2)
-  text("Morning Announcements", width/2, (height/2)+(fontSize+8)*1)
-  text(d, width/2, (height/2)+(fontSize+8)*2)
+    //  MODS
+    modBalls();
+
+    textAlign(CENTER);
+    fill(secMain);
+    text("Laffayette",width/2,height/2 + (fontSize + 8)*-1);
+    text("Morning Announcements",width/2,height/2 + (fontSize+8)*0);
+    text(dispDay[d.getDay()] + ", " + dispMonth[d.getMonth()] + " " + d.getDate(),width/2, height/2 + (fontSize+8)*1)
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth-4,windowHeight-4);
+    borderWidth = floor(width/20);
 }
